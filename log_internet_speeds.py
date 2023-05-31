56b5d1f65dbfdb7c32b14efa7925123b0776b5e9
feeby2494 <toby2494@hotmail.com>
2023-05-31 10:15:21 -0500

    Adding new feature to check internet speeds, but not implemented yet.

---
+++ log_internet_speeds.py
@@ -0,0 +1,8 @@
+
+
+def log_intermet_speeds():
+    """
+        Used to log internet speeds to see if modem connection is suffient and to 
+        use the log file data for data statistics.
+    """
+    pass
